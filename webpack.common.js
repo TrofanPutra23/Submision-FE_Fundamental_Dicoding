32522c61de307e15ecf228881a505c0cd61ab96d
Trofan Putra Pranata <72262798+TrofanPutra23@users.noreply.github.com>
2021-12-02 16:54:50 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -31,8 +31,8 @@ module.exports = {
         new CopyWebpackPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, '/src/img'),
-                    to: path.resolve(__dirname, 'dist/')
+                    from: path.resolve(__dirname, 'src/img'),
+                    to: path.resolve(__dirname, 'dist/img')
                 }
             ]
         })
